279d2c210b463faf73073384ab8ff7bebbd84e64
Yhome <vsamosvat@gmail.com>
2018-04-15 00:11:44 +0400

    added methods' signatures
    added methods' signatures

--- src/main/java/com/stringTests/countSentencesAndWords/App.java
+++ src/main/java/com/stringTests/countSentencesAndWords/App.java
@@ -1,14 +1,29 @@
 package com.stringTests.countSentencesAndWords;
 
+// This program counts sentences and words' frequency from a given text.
+
+
+import com.utils.ConsoleInputsReceiver;
+
 public class App {
+
+    String text = "";
+
     public static void main(String[] args) {
-        App app= new App();
+        App app = new App();
         app.start();
     }
+    private void start() {           //почему static не требует??
 
-    private void start(){           //почему static не требует??
-        String text = new String();
         Storage storage = new Storage(text);
+        Driver driver = new Driver(storage.getText());
+        ConsoleInputsReceiver console = new ConsoleInputsReceiver();
+        System.out.println("Sentences count:" + driver.countSentences());
+        System.out.println("The list of words and their frequency is:\n" + driver.getFrequencies() );
+        System.out.println("Enter the required frequency:");
+        int frequency = console.insistOnIntegerPositiveInput();
+        System.out.println("A word(s) with frequency: " + getByFrequency(frequency));
 
 
     }
+}
--- src/main/java/com/stringTests/countSentencesAndWords/Driver.java
+++ src/main/java/com/stringTests/countSentencesAndWords/Driver.java
@@ -1,4 +1,27 @@
 package com.stringTests.countSentencesAndWords;
 
 public class Driver {
+
+    private String rawText;
+
+    public Driver(String rawText){
+        this.rawText = rawText;
+    }
+
+    public void replaceText (String newText){
+        this.rawText = newText;
+    }
+
+    public int countSentences(){
+        int sentencesCount = 0;
+        return sentencesCount;
+    }
+
+    public String getFrequencies(){
+        return null;
+    }
+
+    public String getByFrequency(int frequency) {
+        return null;
+    }
 }

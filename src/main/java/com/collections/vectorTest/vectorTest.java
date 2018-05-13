77070628d4df7a6b31fd0376016a854692530549
Yhome <vsamosvat@gmail.com>
2018-05-13 23:55:32 +0400

    vectorTest started
    vectorTest started

---
+++ src/main/java/com/collections/vectorTest/vectorTest.java
@@ -0,0 +1,24 @@
+package com.collections.vectorTest;
+
+import java.util.Vector;
+
+public class vectorTest {
+    // Java code illustrating add() method
+        public static void main(String[] args)
+        {
+
+            // create default vector
+            Vector v = new Vector();
+
+
+            v.add(1);
+            v.add(2);
+            v.add("geeks");
+            v.add("forGeeks");
+            v.add(3);
+
+            System.out.println("Vector is " + v);
+
+        }
+    }
+}

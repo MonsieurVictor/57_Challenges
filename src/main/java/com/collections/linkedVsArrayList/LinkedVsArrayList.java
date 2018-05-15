dbe4c802eb706cacbfd727a86e5fd975c26ef39e
Yhome <vsamosvat@gmail.com>
2018-05-15 04:48:31 +0400

    added LinkedVsArrayList
    added LinkedVsArrayList

---
+++ src/main/java/com/collections/linkedVsArrayList/LinkedVsArrayList.java
@@ -0,0 +1,112 @@
+package com.collections.linkedVsArrayList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *        this program compares the speed of ArrayList' and LinkedList' methods "get" and "add"
+ **/
+
+public class LinkedVsArrayList {
+
+    private LinkedList <Integer> linkedList = new LinkedList<>();
+    private ArrayList <Integer> arrayList = new ArrayList<>();
+    private int size = 5000000;
+    private int elementIndex = 100000;
+    private int someNumber = 32134123;
+    private String resultWord;
+
+    private long getTime(){
+        return System.currentTimeMillis();
+    }
+
+    private long getResultTime(long startTime) {
+        return getTime() - startTime;
+    }
+
+    private String compareTime(long arrayTime, long linkedTime) {
+
+        if(arrayTime - linkedTime > 0) {
+            resultWord = "LinkedList";
+        } else if (arrayTime - linkedTime == 0) {
+            resultWord = "they are equal";
+        }
+        else resultWord = "ArrayList";
+        return resultWord;
+    }
+
+    private void createLists(int size){
+
+        for(int i = 0; i < size ; i++){
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+    }
+
+    private void compareAdd() {
+        createLists(size);
+
+        long arrayListTime = getTime();
+        arrayList.add(elementIndex, someNumber);
+
+        long arrayResultTime = getResultTime(arrayListTime);
+
+        System.out.println("Adding element #"+ elementIndex + " of ArrayList with size " + size
+                + " = " + arrayResultTime + " seconds");
+
+        long linkedListTime = getTime();
+        linkedList.add(elementIndex, someNumber);
+
+        long linkedResultTime = getResultTime(linkedListTime);
+
+        System.out.println("Adding element #"+ elementIndex + " of LinkedList with size " + size
+                + " = " + linkedResultTime + " seconds");
+
+        System.out.println("Better is: " + compareTime(arrayResultTime,linkedResultTime));
+        System.out.println();
+
+
+
+    }
+
+    private void compareGet() {
+
+        createLists(size);
+
+        long arrayListTime = getTime();
+        arrayList.get(elementIndex);
+
+        long arrayResultTime = getResultTime(arrayListTime);
+
+
+        System.out.println("Getting element #" + elementIndex + " of ArrayList with size " + size
+                + " = " +  arrayResultTime + " seconds");
+
+        long linkedListTime = getTime();
+        linkedList.get(elementIndex);
+
+        long linkedResultTime = getResultTime(linkedListTime);
+
+
+        System.out.println("Getting element #"+ elementIndex + " of LinkedList with size " + size
+                + " = " + linkedResultTime + " seconds");
+
+        System.out.println("Better is: " + compareTime(arrayResultTime,linkedResultTime));
+        System.out.println();
+
+    }
+
+    public void start() {
+
+        compareAdd();
+        compareGet();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        LinkedVsArrayList linkedVsArrayList = new LinkedVsArrayList();
+        linkedVsArrayList.start();
+    }
+}
--- src/main/java/com/collections/vectorTest/VectorTest.java
+++ src/main/java/com/collections/vectorTest/VectorTest.java
@@ -5,50 +5,91 @@ import java.util.Vector;
 
 public class VectorTest {
     // Java code illustrating add() method
-        public static void main(String[] args)
-        {
+    public static void main(String[] args) {
 
-            // create default vector
-            Vector v = new Vector();
-
-
-            v.add(1);
-            v.add(2);
-            v.add("geeks");
-            v.add("forGeeks");
-            v.add(3);
-
-            System.out.println("Vector is " + v);
-
-            // create default vector
-            Vector vBoolean = new Vector();
+        // create default vector
+        Vector v = new Vector();
 
 
-            vBoolean.add(0, 1);
-            vBoolean.add(1, 2);
-            vBoolean.add(2, "geeks");
-            vBoolean.add(3, "forGeeks");
-            vBoolean.add(4, 3);
+        v.add(1);
+        v.add(2);
+        v.add("geeks");
+        v.add("forGeeks");
+        v.add(3);
 
-            System.out.println("Vector is " + vBoolean);
+        System.out.println("Vector is " + v);
 
-            ArrayList arr = new ArrayList();
-            arr.add(3);
-            arr.add("geeks");
-            arr.add("forgeeks");
-            arr.add(4);
+        // create default vector
+        Vector vBoolean = new Vector();
 
 
-            // createn default vector
-            Vector vAddAll = new Vector();
+        vBoolean.add(0, 1);
+        vBoolean.add(1, 2);
+        vBoolean.add(2, "geeks");
+        vBoolean.add(3, "forGeeks");
+        vBoolean.add(4, 3);
+
+        System.out.println("Vector is " + vBoolean);
+
+        ArrayList arr = new ArrayList();
+        arr.add(3);
+        arr.add("geeks");
+        arr.add("forgeeks");
+        arr.add(4);
 
 
-            // copying all element of array list int0 vector
-            vAddAll.addAll(arr);
+        // createn default vector
+        Vector vAddAll = new Vector();
 
-            // checking vector v
-            System.out.println("vector v:" + vAddAll);
 
-        }
+        // copying all element of array list int0 vector
+        vAddAll.addAll(arr);
+
+        // checking vector v
+        System.out.println("vector v:" + vAddAll);
+
+
+
+
+
+
+
+        ArrayList arrCollection = new ArrayList();
+        arrCollection.add(3);
+        arrCollection.add("geeks");
+        arrCollection.add("forgeeks");
+        arrCollection.add(4);
+
+
+        // createn default vector
+        Vector vCollection = new Vector();
+
+        vCollection.add(2);
+        // copying all element of array list int0 vector
+        vCollection.addAll(1, arrCollection);
+
+        // checking vector v
+        System.out.println("vector v:" + vCollection);
+
+
+
+
+        Vector vClear = new Vector();
+
+
+        vClear.add(0, 1);
+        vClear.add(1, 2);
+        vClear.add(2, "geeks");
+        vClear.add(3, "forGeeks");
+        vClear.add(4, 3);
+
+        System.out.println("Vector is: " + vClear);
+
+        //clearing the vector
+        vClear.clear();
+
+        // checking vector
+        System.out.println("after clearing: " + vClear);
+
     }
 }

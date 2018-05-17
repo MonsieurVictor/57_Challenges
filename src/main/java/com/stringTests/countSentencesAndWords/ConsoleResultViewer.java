5efda7374bd9356d528def11f2a9ddaa4b3186c5
Yhome <vsamosvat@gmail.com>
2018-05-18 00:31:04 +0400

    refactored AppOptions
    refactored AppOptions

--- src/main/java/com/stringTests/countSentencesAndWords/ConsoleResultViewer.java
+++ src/main/java/com/stringTests/countSentencesAndWords/ConsoleResultViewer.java
@@ -9,30 +9,28 @@ public class ConsoleResultViewer implements IResultViewer {
     private IAppOptions options;
     private ITextAnalyzer analyzer;
 
-//    public void setOptions() {
-//        this.options = analyzer.getOptions();   // передали опции. грамотно ли?
-//    }
-
     public void setOptions(IAppOptions options) {
-
+        this.options = options;
     }
 
     public void report(ITextAnalyzer analyzer) {
         this.analyzer = analyzer;
+        setOptions(this.analyzer.getOptions());
         if (options.isIgnoreListEnabled()){
             System.out.println("Ignored words :" + analyzer.getIgnoreList());
         }
 
-        if (options.isStatsEnabled()){
+        if (options.isStatsEnabled()) {
             System.out.println("Sentences count: " + analyzer.getSentencesCount());
             System.out.println("Total words count : " + analyzer.getTotalWordsCount());
             System.out.println("Unique words count: " + analyzer.getUniqueWordsCount());
         }
 
-        if (options.isFreqEnabled()){
+        if (options.isFreqEnabled()) {
             System.out.println("Words frequencies and in how many sentences encountered: ");
-            for (int i = 0;i < analyzer.getTrioSize(); i++)
-            System.out.println (analyzer.getTrioWord(i)+ " : " + analyzer.getTrioFreq(i) + " , " + analyzer.getTrioAppear(i));
+            for (int i = 0;i < this.analyzer.getTrioSize(); i++)
+            System.out.println (this.analyzer.getTrioWord(i) + " : "
+                    + this.analyzer.getTrioFreq(i) + " , " + this.analyzer.getTrioAppear(i));
         }
     }
 }

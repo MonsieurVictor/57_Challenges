8bb83b634e0d9f6d32bd26c373b96f494f0453e8
tymiurge <tmrlabas@gmail.com>
2018-05-17 14:16:24 +0300

    pondering on String, StringBuffer and StringBuilder

--- src/main/java/com/stringTests/stringBuilderBufferDifference/mainDifference.java
+++ src/main/java/com/stringTests/stringBuilderBufferDifference/mainDifference.java
@@ -1,13 +1,27 @@
 package com.stringTests.stringBuilderBufferDifference;
 
+/**
+ *  Java program to demonstrate difference between
+ *      - String,
+ *      - StringBuilder
+ *      - StringBuffer
+ *
+ *      Mutability Difference:
+ *          String - immutable, StringBuilder and StringBuffer - mutable
+ *              >> String is good for a map keys
+ *              >> StringBuilder & StringBuffer are more efficient for operations like concat, find, replace etc etc
+ *
+ *      Thread-Safety Difference:
+ *          StringBuffer is thread-safe when StringBuilder is not thread-safe
+ *              >> So when the application needs to be run only in a single thread then it is better to use StringBuilder
+ */
 public class mainDifference {
 
-    // Java program to demonstrate difference between String,
-// StringBuilder and StringBuffer
 
     public static void concat1(String s1) {  // Concatenates to String
-
-        s1 = s1 + "forgeeks";
+        System.out.println("s1 hash code before an attempt to change: " + s1.hashCode());
+        s1 += "forgeeks";
+        System.out.println("s1 hash code after an attempt to change: " + s1.hashCode());
     }
 
     // Concatenates to StringBuilder
@@ -24,8 +38,10 @@ public class mainDifference {
 
     public static void main(String[] args) {
         String s1 = "Geeks";
+        System.out.println("s1 hash code: " + s1.hashCode());
         concat1(s1);  // s1 is not changed
         System.out.println("String: " + s1);
+        System.out.println("s1 hash code: " + s1.hashCode());
 
         StringBuilder s2 = new StringBuilder("Geeks");
         concat2(s2); // s2 is changed

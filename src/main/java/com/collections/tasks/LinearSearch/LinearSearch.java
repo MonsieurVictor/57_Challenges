b70bd90eb464489a568cbd771bc9ccfa3c8537a1
Yhome <vsamosvat@gmail.com>
2018-05-22 23:56:44 +0300

    LinerarSearch drunk edition

--- src/main/java/com/collections/tasks/LinearSearch/LinearSearch.java
+++ src/main/java/com/collections/tasks/LinearSearch/LinearSearch.java
@@ -1,5 +1,26 @@
 package com.collections.tasks.LinearSearch;
 
 public class LinearSearch {
-    
+    public static void main(String[] args) {
+        Integer i1 = 1;
+        Integer i2 = 2;
+        Integer[] list;
+        for (Integer integer : list = new Integer[](1,2)) {
+            integer = (int) Math.random();
+        }
+
+        search(1, list);
+    }
+
+        public static int search(Integer n, Integer[] list) {
+
+            int index = -1;
+            for (int i = 0; i < list.length; i++) {
+                if (list[i].equals(n)) {
+                index = i;
+                break;
+                }
+            }
+            return index;
+    }
 }
---
+++ src/main/java/com/collections/tasks/basicStream/BasicStream.java
@@ -0,0 +1,20 @@
+package com.collections.tasks.basicStream;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicStream {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("one");
+        list.add("two");
+        list.stream().filter(element -> {
+            System.out.println(element);
+            return element.equals("two");
+        }).count() ;
+
+        list.stream().forEach(x -> System.out.println(x) );
+    }
+}

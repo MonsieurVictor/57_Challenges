4aed4b24a61cc2edf8907abeafc9e889e05ea28d
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-01 20:47:52 -1100

    some fixes
    some fixes

--- src/main/java/com/chapter4/java10_Examples/Java10Examples.java
+++ src/main/java/com/chapter4/java10_Examples/Java10Examples.java
@@ -26,65 +26,60 @@ SOFTWARE.
 ==============================================================================
 **//* Yuck! */
 
-class Gen { }
-class Gen1 <Gen> { }
-class Gen2 <Gen1, String> { }
-class Gen3 <Gen1, Gen2> { }
 
-public class Java10_Examples {
-    public static void main (String[] args) {
+public class Java10Examples {
+    public static void main(String[] args) {
 
         //Example #1
         var one = "Type Inference!"; //Inferred to java.lang.String;
         out.println("\nOur first use of var!:\t\"" + one + "\"");
         out.println("The variable is of/can cast to type " +
-                ((Object)one).getClass().getName() + ".\n");
+                ((Object) one).getClass().getName() + ".\n");
 
         //Example #2
         var two = 9; //Inferred to int
         out.println("\nOur second use of var!:\t\"" + two + "\"");
         out.println("The variable is of/can cast to type " +
-                ((Object)two).getClass().getName() + ".\n");
+                ((Object) two).getClass().getName() + ".\n");
 
         //Example #2
         var three = 9.9; //Inferred to double
         out.println("\nOur second use of var!:\t\"" + three + "\"");
         out.println("The variable is of/can cast to type " +
-                ((Object)three).getClass().getName() + ".\n");
+                ((Object) three).getClass().getName() + ".\n");
 
         //Example #4 - The evolution of messy generic instantiation
-        Gen3 <Gen1<Gen>, Gen2<Gen1,String>> gen3_1 = new Gen3 <Gen1<Gen>, Gen2<Gen1,String>> (); //88 Characters
-        Gen3 <Gen1<Gen>, Gen2<Gen1,String>> gen3_2 = new Gen3 <> (); //60 Characters
-        var gen3_3 = new Gen3 <Gen1<Gen>, Gen2<Gen1,String>> (); // 56 Characters
-        var four = new Gen3 <> (); //26 Characters
+        Gen3<Gen1<Gen>, Gen2<Gen1, String>> gen3_1 = new Gen3<Gen1<Gen>, Gen2<Gen1, String>>(); //88 Characters
+        Gen3<Gen1<Gen>, Gen2<Gen1, String>> gen3_2 = new Gen3<>(); //60 Characters
+        var gen3_3 = new Gen3<Gen1<Gen>, Gen2<Gen1, String>>(); // 56 Characters
+        var four = new Gen3<>(); //26 Characters
         out.println("\nOur fourth use of var!:\t\"" + four + "\"");
         out.println("The variable is of/can cast to type " +
-                ((Object)four).getClass().getName() + ".\n");
+                ((Object) four).getClass().getName() + ".\n");
         out.println("In Java 5-6 this generic code was 88 characters long.");
         out.println("In Java 7-9 this generic code is 60 characters long.");
         out.println("In Java 10 this generic code can be 26 characters long.\n");
 
         // #Example #5 - Common Collections
-        var five = new java.util.ArrayList<String> ();
+        var five = new java.util.ArrayList<String>();
         five.add("Hello!");
         five.add("Hiya!");
         out.println("\nOur fifth use of var!:\t\"" + five + "\"");
         out.println("The variable is of/can cast to type " +
-                ((Object)five).getClass().getName() + ".\n");
+                ((Object) five).getClass().getName() + ".\n");
+    }
+
+
+    private class Gen {
+    }
+
+    private class Gen1<Gen> {
+    }
+
+    private class Gen2<Gen1, String> {
+    }
+
+    private class Gen3<Gen1, Gen2> {
     }
 }
 
-
-© 2018 GitHub, Inc.
-        Terms
-        Privacy
-        Security
-        Status
-        Help
-        Contact GitHub
-        API
-        Training
-        Shop
-        Blog
-        About
-        Press h to open a hovercard with more details.

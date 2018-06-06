1f0775377078ba21363099c4119c9708a807b618
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-06 22:19:28 +0100

    added some new features to java9Features
    added some new features to java9Features

--- src/main/java/com/collections/java9Features/Java9Features.java
+++ src/main/java/com/collections/java9Features/Java9Features.java
@@ -1,11 +1,17 @@
 package com.collections.java9Features;
 
 
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
@@ -15,46 +21,78 @@ import java.util.stream.Stream;
 
         private interface Card {
 
-    //    Private methods in Interfaces
-    // In Java 8, we can provide method implementation in Interfaces using Default and Static methods. However we cannot create private methods in Interfaces.
-    // To avoid redundant code and more re-usability, Oracle Corp is going to introduce private methods in Java SE 9 Interfaces. From Java SE 9 on-wards, we can write private and private static methods too in an interface using ‘private’ keyword.
-    // These private methods are like other class private methods only, there is no difference between them.
+            //    Private methods in Interfaces
+            // In Java 8, we can provide method implementation in Interfaces using Default and Static methods.
+            // However we cannot create private methods in Interfaces.
+            // To avoid redundant code and more re-usability, Oracle Corp is going to introduce private methods in Java SE 9 Interfaces.
+            // From Java SE 9 on-wards, we can write private and private static methods too in an interface using ‘private’ keyword.
+            // These private methods are like other class private methods only, there is no difference between them.
 
             private Long createCardID() {
-                // Method implementation goes here.
+            // Method implementation goes here.
                 return null;
             }
 
             private static void displayCardDetails() {
-                // Method implementation goes here.
+            // Method implementation goes here.
             }
         }
 
-        public static void main(String[] args) throws FileNotFoundException {
+        public static void main(String[] args) throws IOException, InterruptedException {
+
+            // The Streams API is arguably one of the best improvements to the Java standard library in a long time.
+            // It allows you to create declarative pipelines of transformations on collections. With Java 9, this only gets better.
+            // There are four new methods added to the Stream interface: dropWhile, takeWhile, ofNullable.
+            // The iterate method gets a new overload, allowing you to provide a Predicate on when to stop iterating:
+            // The second argument is a lambda that returns true until the current element in the IntStream becomes 100.
+            // This simple example therefore prints the integers 1 until 99 on the console.
+
+            IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
+
+            // Besides these additions on Stream itself, the integration between Optional and Stream has been improved.
+            // It's now possible to turn an Optional object into a (possibly empty) Stream with the new `stream` method on Optional:
+            // Turning an Optional into a Stream is especially useful when composing complex Stream pipelines.
+
+            Stream<Integer> s = Optional.of(1).stream();
+
+            // A new way of performing HTTP calls arrives with Java 9.
+            // This much overdue replacement for the old `HttpURLConnection` API also supports WebSockets and HTTP/2 out of the box.
+            // One caveat: The new HttpClient API is delivered as a so-called _incubator module_ in Java 9.
+            // This means the API isn't guaranteed to be 100% final yet.
+            // Still, with the arrival of Java 9 you can already start using this API:
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest req =
+                    HttpRequest.newBuilder(URI.create("http://www.google.com"))
+                            .header("User-Agent","Java")
+                            .GET()
+                            .build();
 
 
-//    Empty List Example
+            HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandler.asString());
+            //    Empty List Example
 
             List immutableList = List.of();
-//    Non-Empty List Example
+            //    Non-Empty List Example
 
             List immutableList2 = List.of("one", "two", "three");
             System.out.println("List immutableList2 = List.of(\"one\", \"two\", \"three\"): " + immutableList2);
 
-//    Stream API Improvements
-//    In Java SE 9, Oracle Corp has added four useful new methods to java.util.Stream interface. As Stream is an interface, all those new implemented methods are default methods. Two of them are very important: dropWhile and takeWhile methods
-//    If you are familiar with Scala Language or any Functions programming language, you will definitely know about these methods. These are very useful methods in writing some functional style code. Let us discuss about takeWhile utility method here.
-//    This takeWhile() takes a predicate as an argument and returns a Stream of subset of the given Stream values until that Predicate returns false for the first time. If first value does NOT satisfy that Predicate, it just returns an empty Stream.
+            //    Stream API Improvements
+            //    In Java SE 9, Oracle Corp has added four useful new methods to java.util.Stream interface. As Stream is an interface, all those new implemented methods are default methods. Two of them are very important: dropWhile and takeWhile methods
+            //    If you are familiar with Scala Language or any Functions programming language, you will definitely know about these methods. These are very useful methods in writing some functional style code. Let us discuss about takeWhile utility method here.
+            //    This takeWhile() takes a predicate as an argument and returns a Stream of subset of the given Stream values until that Predicate returns false for the first time. If first value does NOT satisfy that Predicate, it just returns an empty Stream.
 
             Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).takeWhile(i -> i < 5)
                     .forEach(System.out::println);
-        }
-//    Try With Resources Improvement
-//    We know, Java SE 7 has introduced a new exception handling construct: Try-With-Resources to manage resources automatically.
-//    The main goal of this new statement is “Automatic Better Resource Management”.
-//    Java SE 9 is going to provide some improvements to this statement to avoid some more verbosity and improve some Readability.
-//
-//    Java SE 7 example
+            }
+            //    Try With Resources Improvement
+            //    We know, Java SE 7 has introduced a new exception handling construct: Try-With-Resources to manage resources automatically.
+            //    The main goal of this new statement is “Automatic Better Resource Management”.
+            //    Java SE 9 is going to provide some improvements to this statement to avoid some more verbosity and improve some Readability.
+            //
+            //    Java SE 7 example
 
         void testARM_Before_Java9() throws IOException {
             BufferedReader reader1 = new BufferedReader(new FileReader("journaldev.txt"));

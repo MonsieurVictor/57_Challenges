47e0ed0481fcd0f645a08c64139793341ac92cc7
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-16 00:59:21 +0300

    added methods of object class to a functional interface
    added methods of object class to a functional interface

--- src/main/java/com/collections/java8FuncInterface/java8FuncInterface.java
+++ src/main/java/com/collections/java8FuncInterface/java8FuncInterface.java
@@ -1,9 +1,22 @@
 package com.collections.java8FuncInterface;
+/**
+ An Interface that contains exactly one abstract method is known as functional interface.
+ It can have any number of default, static methods but can contain only one abstract method.
+ It can also declare methods of object class.
+ Functional Interface is also known as Single Abstract Method Interfaces or SAM Interfaces.
+ It is a new feature in Java, which helps to achieve functional programming approach.
+**/
 
 @FunctionalInterface
+ //A functional interface can have methods of object class. See in the following example.
 interface sayable{
-    void say(String msg);
+    void say(String msg);   // abstract method
+    // It can contain any number of Object class methods.
+    int hashCode();
+    String toString();
+    boolean equals(Object obj);
 }
+
 public class java8FuncInterface implements sayable{
     public void say(String msg){
         System.out.println(msg);

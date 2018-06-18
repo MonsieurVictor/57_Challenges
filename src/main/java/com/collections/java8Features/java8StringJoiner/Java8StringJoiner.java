818d43846f08139381bdc617b08401535f7244cc
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-19 01:29:59 +0300

    refactored java8StringJoiner
    refactored java8StringJoiner

--- src/main/java/com/collections/java8Features/java8StringJoiner/Java8StringJoiner.java
+++ src/main/java/com/collections/java8Features/java8StringJoiner/Java8StringJoiner.java
@@ -26,6 +26,64 @@ public class Java8StringJoiner {
         joinNames2.add("Raheem");
 
         System.out.println(joinNames2);
+
+        /**     StringJoiner Methods
+         */
+        StringJoiner joinNames3 = new StringJoiner(","); // passing comma(,) as delimiter
+
+        // Prints nothing because it is empty
+        System.out.println(joinNames3);
+
+        // We can set default empty value.
+        joinNames3.setEmptyValue("It is empty");
+        System.out.println(joinNames3);
+
+
+        // Adding values to StringJoiner
+        joinNames3.add("Rahul");
+        joinNames3.add("Raju");
+        System.out.println(joinNames3);
+
+        // Returns length of StringJoiner
+        int length = joinNames3.length();
+        System.out.println("Length: "+length);
+
+        // Returns StringJoiner as String type
+        String str = joinNames3.toString();
+        System.out.println(str);
+
+        // Now, we can apply String methods on it
+        char ch = str.charAt(3);
+        System.out.println("Character at index 3: "+ch);
+
+        // Adding one more element
+        joinNames3.add("Sorabh");
+        System.out.println(joinNames3);
+
+        // Returns length
+        int newLength = joinNames3.length();
+        System.out.println("New Length: "+newLength);
+
+
+        /**Merge Two StringJoiner
+        **/
+
+        StringJoiner joinNames4 = new StringJoiner(",", "[", "]");   // passing comma(,) and square-brackets as delimiter
+
+        // Adding values to StringJoiner
+        joinNames4.add("Rahul");
+        joinNames4.add("Raju");
+
+        // Creating StringJoiner with :(colon) delimiter
+        StringJoiner joinNames5 = new StringJoiner(":", "[", "]");  // passing colon(:) and square-brackets as delimiter
+
+        // Adding values to StringJoiner
+        joinNames5.add("Peter");
+        joinNames5.add("Raheem");
+
+        // Merging two StringJoiner
+        StringJoiner merge = joinNames4.merge(joinNames5);
+        System.out.println(merge);
     }
 }
 

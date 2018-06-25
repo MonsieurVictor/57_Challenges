fedb3a803c6c93204a66b7abeba7a382c4313b55
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-26 01:13:16 +0300

    refactored Java8TypeInference
    refactored Java8TypeInference

--- src/main/java/com/collections/java8Features/java8TypeInference/Java8TypeInference.java
+++ src/main/java/com/collections/java8Features/java8TypeInference/Java8TypeInference.java
@@ -15,13 +15,44 @@ import java.util.List;
 
 public class Java8TypeInference {
 
-    public static void showList(List<Integer> list){
-        if(!list.isEmpty()){
-            list.forEach(System.out::println);
-        }else System.out.println("list is empty");
+/** You can also create your own custom generic
+ * class and methods.
+ * In the following example,
+ * we are creating our own generic
+ * class and method.
+ **/
+
+    static class GenericClass<X> {
+        X name;
+        public void setName(X name){
+            this.name = name;
+        }
+        public X getName(){
+            return name;
+        }
+        public String genericMethod(GenericClass<String> x){
+            x.setName("John");
+            return x.name;
+        }
     }
 
     public static void main(String[] args) {
+        GenericClass<String> genericClass = new GenericClass<String>();
+        genericClass.setName("Peter");
+        System.out.println(genericClass.getName());
+
+        GenericClass<String> genericClass2 = new GenericClass<>();
+        genericClass2.setName("peter");
+        System.out.println(genericClass2.getName());
+
+        // New improved type inference
+        System.out.println(genericClass2.genericMethod(new GenericClass<>()));
+
+      /**
+       * example 2:
+       * You can use type inference with generic classes and methods.
+       * */
+
         // An old approach(prior to Java 7) to create a list
         List<Integer> list1 = new ArrayList<Integer>();
         list1.add(11);
@@ -33,5 +64,14 @@ public class Java8TypeInference {
         // Compiler infers type of ArrayList, in Java 8
         showList(new ArrayList<>());
     }
+
+        public static void showList(List<Integer> list6){
+    if(!list6.isEmpty()){
+        list6.forEach(System.out::println);
+    }else System.out.println("list is empty");
+}
+
+
+
 }
 

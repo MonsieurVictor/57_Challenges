7e18f421972164a2cffbfe66a79d2b77fd325980
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-06-30 01:59:20 +0300

    refactored Java9SafeVarArgs
    refactored Java9SafeVarArgs

--- src/main/java/com/collections/java9Features/java9SafeVarArgs/Java9SafeVarArgs.java
+++ src/main/java/com/collections/java9Features/java9SafeVarArgs/Java9SafeVarArgs.java
@@ -11,20 +11,45 @@ import java.util.List;
  * From Java 9, it can also be used with private instance methods.
  **/
 
+
+
 public class Java9SafeVarArgs {
 
     // Applying @SaveVarargs annotation
+
     @SafeVarargs
     private void display(List<String>... products) { // Not using @SaveVarargs
         for (List<String> product : products) {
             System.out.println(product);
         }
     }
+
     public static void main(String[] args) {
-        Java9SafeVarArgs p = new Java9SafeVarArgs();
+
+        UnsafeVarArgs unsafeVarArgs = new UnsafeVarArgs();
+        List<String> unsafeList = new ArrayList<String>();
+        unsafeList.add("Unsafe Laptop");
+        unsafeList.add("Unsafe Tablet");
+        unsafeVarArgs.display(unsafeList);
+
+/**    It produces warning messages at compile time, but compiles without errors.
+
+
+       To avoid it, we should use @SaveVarargs notation to the method, as we did in the following example.
+ */
+
+        Java9SafeVarArgs safeVarArgs = new Java9SafeVarArgs();
         List<String> list = new ArrayList<String>();
         list.add("Laptop");
         list.add("Tablet");
-        p.display(list);
+        safeVarArgs.display(list);
+    }
+
+    private static class UnsafeVarArgs {
+        private void display(List<String>... products) { // Not using @SaveVarargs
+            for (List<String> product : products) {
+                System.out.println(product);
+            }
+        }
     }
 }

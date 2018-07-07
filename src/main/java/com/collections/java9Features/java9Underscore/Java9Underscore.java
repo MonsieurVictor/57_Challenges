56cfbc9a41ce4d18e170275f51e08c094c3b9ba8
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-07-07 14:07:29 +0300

    refactored Java9VersionString
    refactored Java9VersionString

---
+++ src/main/java/com/collections/java9Features/java9Underscore/Java9Underscore.java
@@ -0,0 +1,8 @@
+package com.collections.java9Features.java9Underscore;
+
+public class Java9Underscore {
+    public static void main(String[] args) {
+    int _ = 10;
+    System.out.println(_);
+    }
+}
--- src/main/java/com/collections/java9Features/java9VersionString/Java9VersionString.java
+++ src/main/java/com/collections/java9Features/java9VersionString/Java9VersionString.java
@@ -1,5 +1,12 @@
 package com.collections.java9Features.java9VersionString;
 
+/**
+ Java version-string is a format that contains version specific information.
+ This version-string consists of major, minor, security and patch update releases.
+ In Java 9, a new version-string scheme is introduced that looks like the below.
+ $MAJOR.$MINOR.$SECURITY.$PATCH
+ */
+
 public class Java9VersionString {
     public static void main(String[] args) {
         Runtime.Version version = Runtime.version();                 // Getting runtime version instance

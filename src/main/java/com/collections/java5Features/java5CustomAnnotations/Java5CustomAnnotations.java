dce407f56b33434603ee19ae2eafcd27d9ea4331
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-07-29 10:41:54 -1200

    started Java5CustomAnnotations
    started Java5CustomAnnotations

---
+++ src/main/java/com/collections/java5Features/java5CustomAnnotations/Java5CustomAnnotations.java
@@ -0,0 +1,25 @@
+package com.collections.java5Features.java5CustomAnnotations;
+
+/** Java Custom annotations or Java User-defined annotations are easy to create and use.
+ * The @interface element is used to declare an annotation. For example:
+
+@interface MyAnnotation{}
+Here, MyAnnotation is the custom annotation name.
+
+        There are few points that should be remembered by the programmer:
+
+        -Method should not have any throws clauses
+        -Method should return one of the following: primitive data types, String, Class, enum or array of these data types.
+        -Method should not have any parameter.
+        -We should attach @ just before interface keyword to define annotation.
+        -It may assign a default value to the method.
+
+        There are three types of annotations:
+
+        Marker Annotation
+        Single-Value Annotation
+        Multi-Value Annotation
+ **/
+
+public class Java5CustomAnnotations {
+}
